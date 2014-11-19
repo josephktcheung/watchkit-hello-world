a99c59cf88323a55036a4a1562f0f2726ce4dc12
josephktcheung <josephktcheung@gmail.com>
2014-11-19 10:19:48 +0800

    Hello world Watch Kit

---
+++ watchkit hello world WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,35 @@
+//
+//  InterfaceController.swift
+//  watchkit hello world WatchKit Extension
+//
+//  Created by Joseph Cheung on 19/11/14.
+//  Copyright (c) 2014 josephktcheung. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override init(context: AnyObject?) {
+        // Initialize variables here.
+        super.init(context: context)
+        
+        // Configure interface objects here.
+        NSLog("%@ init", self)
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        NSLog("%@ will activate", self)
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        NSLog("%@ did deactivate", self)
+        super.didDeactivate()
+    }
+
+}
